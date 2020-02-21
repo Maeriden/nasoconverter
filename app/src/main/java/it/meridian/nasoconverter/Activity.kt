f9c91e82454dd1a198c0ce38c1ebc066a4a03e9b
Daniele Bondì <daniele.m.bondi@gmail.com>
2020-02-21 16:42:12 +0100

    Coin names Coin icon size represents coin size Cannot uncheck Legni in conversion from $ Larger text fields
    Removed unused test libraries dependencies
    Enabled minification

--- app/src/main/java/it/meridian/nasoconverter/Activity.kt
+++ app/src/main/java/it/meridian/nasoconverter/Activity.kt
@@ -1,9 +1,15 @@
 package it.meridian.nasoconverter
 
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import java.lang.IllegalArgumentException
+
 
 class Activity
     : FragmentActivity()
@@ -18,5 +24,24 @@ class Activity
 
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+    }
+
+
+    private class AdapterViewPager(private val context: Context, fragmentManager: FragmentManager)
+        : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+    {
+        override fun getItem(position: Int) : Fragment = when(position) {
+            0 -> FragmentMoney2Coins()
+            1 -> FragmentCoins2Money()
+            else -> throw IllegalArgumentException()
+        }
+
+        override fun getPageTitle(position: Int) : CharSequence? = when(position) {
+            0 -> this.context.getString(R.string.tab_title_0)
+            1 -> this.context.getString(R.string.tab_title_1)
+            else -> throw IllegalArgumentException()
+        }
+
+        override fun getCount() : Int = 2
     }
 }
--- app/src/main/java/it/meridian/nasoconverter/ToggleCoin.kt
+++ app/src/main/java/it/meridian/nasoconverter/ToggleCoin.kt
@@ -8,16 +8,20 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.*
 
+
 class ToggleCoin(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
     : ImageView(context, attributeSet, defStyleAttr, defStyleRes)
 {
     private var callback  : ((view : ToggleCoin, isChecked : Boolean) -> Unit)? = null
-    private var checked : Boolean
+    private var checkable : Boolean
+    private var checked   : Boolean
+
 
     init
     {
         val attrs: TypedArray? = context.obtainStyledAttributes(attributeSet, R.styleable.ToggleCoin, defStyleAttr, defStyleRes)
-        this.checked = attrs?.getBoolean(R.styleable.ToggleCoin_checked, false) ?: false
+        this.checkable = attrs?.getBoolean(R.styleable.ToggleCoin_checkable, false) ?: false
+        this.checked   = attrs?.getBoolean(R.styleable.ToggleCoin_checked, false) ?: false
         attrs?.recycle()
 
         this.applyTint()
@@ -37,24 +41,17 @@ class ToggleCoin(context: Context, attributeSet: AttributeSet?, defStyleAttr: In
     private fun applyTint()
     {
         if(this.checked)
-        {
             this.setColorFilter(Color.rgb(64, 64, 64), PorterDuff.Mode.MULTIPLY)
-//            this.imageTintList = ColorStateList.valueOf(Color.argb(127, 0, 0, 0))
-//            this.imageTintMode = PorterDuff.Mode.SRC_OVER
-        }
         else
-        {
             this.clearColorFilter()
-//            this.imageTintList = null
-//            this.imageTintMode = PorterDuff.Mode.SRC
-        }
-//        this.invalidate()
     }
 
 
     @Suppress("UNUSED_PARAMETER")
     private fun onClick(view : View)
     {
+        if(!this.checkable)
+            return
         this.checked = !this.checked
         this.applyTint()
         this.callback?.invoke(this, this.checked)
